bce509cc135c9848773ed9250157ee929317d434
AfaqJamshaid <f190364@cfd.nu.edu.pk>
2022-01-18 12:35:02 +0500

    Commited layouts for frontend

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white mt-5 pd-4 text-center">
+      Copyright &copy; {new Date().getFullYear()} <a href="https://github.com/starkblaze01/Hostel-Management#team-members">Team Web Tech</a>
+    </footer>
+  );
+}
